23266a3df12fbf7c8e73a2e4df8101d8282adfba
zube <zubcic.toni@hotmail.com>
2022-04-13 15:06:44 +0200

    Added Hardware model and mocked hardware list

---
+++ src/app/hardware/mocked-hardware-list.ts
@@ -0,0 +1,7 @@
+import {Hardware} from "./hardware-model";
+
+export const MockedHardwareList: Hardware[] = [
+  { name: "ASUS RTX3060", price: 5000},
+  { name: "ASUS GTX1660", price: 3500},
+  { name: "ASUS Gigabyte RTX3050", price: 3000},
+]
