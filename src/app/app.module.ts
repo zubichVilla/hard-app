b527fe19247f0e269a9da7e785e9c9344d696ea7
zube <zubcic.toni@hotmail.com>
2022-04-13 17:09:00 +0200

    Added bootstrap and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HardwareComponent } from './hardware/hardware.component';
 import { HardwareDetailComponent } from './hardware/hardware-detail/hardware-detail.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HardwareDetailComponent } from './hardware/hardware-detail/hardware-det
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
