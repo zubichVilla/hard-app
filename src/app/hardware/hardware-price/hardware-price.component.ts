e405e9bfc63b8d0479713b0914bcdff23f0bee4d
zube <zubcic.toni@hotmail.com>
2022-04-14 18:30:24 +0200

    Added hardware-price component

--- src/app/hardware/hardware-price/hardware-price.component.ts
+++ src/app/hardware/hardware-price/hardware-price.component.ts
@@ -14,4 +14,17 @@ export class HardwarePriceComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  condition(price: number) {
+
+    if(price < 100){
+       return "Ovaj hardver se ne može kupiti na rate."
+    }else if(price >= 100 && price < 400 ){
+       return "Ovaj hardver se može kupiti na 6 rata."
+    }else if(price >= 400 && price < 800){
+       return "Ovaj hardver se može kupiti na 12 rata."
+    }else{
+      return "Ovaj hardver se može kupiti na 24 rate."
+    }
+
+  }
 }
