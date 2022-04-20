ac5ed3019f8bf92fc8cf661d4905dcd89de46ab5
zube <zubcic.toni@hotmail.com>
2022-04-20 13:04:47 +0200

    Added a button that generates a list of products
    Clicking on a hardware button, the hardware.component view is generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HardwareComponent} from "./hardware/hardware.component";
+import {HardwareDetailComponent} from "./hardware/hardware-detail/hardware-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'hardware', component: HardwareComponent },
+  { path: 'detail/:code', component: HardwareDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hard-app';
+  title = 'Hardware app';
+  // title = 'hard-app';
 }
